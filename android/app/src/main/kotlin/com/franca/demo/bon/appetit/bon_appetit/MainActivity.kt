d602404a983fc8ac8c98628978837c0582797ec1
neretzes <caboyof@gmail.com>
2022-02-04 16:09:19 +0800

    Skeleton

---
+++ android/app/src/main/kotlin/com/franca/demo/bon/appetit/bon_appetit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.franca.demo.bon.appetit.bon_appetit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Bon Appetit',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bon Appetit'),
+      ),
+      body: Container(),
+    );
+  }
+}
