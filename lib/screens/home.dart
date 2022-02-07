abb99413ba753c8323ea36f085a6b286f25f8788
neretzes <caboyof@gmail.com>
2022-02-07 15:53:37 +0800

    Images and fonts

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -24,7 +24,77 @@ class HomePage extends StatelessWidget {
           height: 50.0,
         ),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        child: Column(
+          children: const [
+            Text(
+              'Radley Regular',
+              style: TextStyle(
+                fontFamily: 'Radley',
+                fontSize: 32,
+                fontStyle: FontStyle.normal,
+              ),
+            ),
+            Text(
+              'Radley Italic',
+              style: TextStyle(
+                fontFamily: 'Radley',
+                fontSize: 32,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            Text(
+              'Futura PT Light',
+              style: TextStyle(
+                fontFamily: 'Futura PT',
+                fontSize: 32,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            Text(
+              'Futura PT Regular',
+              style: TextStyle(
+                fontFamily: 'Futura PT',
+                fontSize: 32,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            Text(
+              'Futura PT Medium',
+              style: TextStyle(
+                fontFamily: 'Futura PT',
+                fontSize: 32,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Text(
+              'Futura PT Semi-bold',
+              style: TextStyle(
+                fontFamily: 'Futura PT',
+                fontSize: 32,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Text(
+              'Futura PT Bold',
+              style: TextStyle(
+                fontFamily: 'Futura PT',
+                fontSize: 32,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Text(
+              'the red fox jumped over the lazy dog',
+              style: TextStyle(
+                fontFamily: 'Futura PT',
+                fontSize: 32,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/today_recipes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TodayRecipesWidget extends StatelessWidget {
+  const TodayRecipesWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
