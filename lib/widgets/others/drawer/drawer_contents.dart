bf0e061e919e4e98de8d05d625257c1efda29f7a
neretzes <caboyof@gmail.com>
2022-03-04 15:48:59 +0800

    Preparation for Drawer routing

--- lib/widgets/others/drawer/drawer_contents.dart
+++ lib/widgets/others/drawer/drawer_contents.dart
@@ -1,3 +1,4 @@
+import 'package:bon_appetit/models/index.dart';
 import 'package:flutter/material.dart';
 
 import 'drawer_account.dart';
@@ -22,24 +23,24 @@ class DrawerContentsWidget extends StatelessWidget {
     final widgets = <Widget>[];
 
     for (final route in routes) {
-      widgets.add(DrawerRouterWidget(route));
+      widgets.add(DrawerRouterWidget(route.text));
     }
 
     return widgets;
   }
 
-  // TODO: Create blank pages for each and convert the return type from List<String> to Map<String, PageRouteInfo>
-  List<String> get routes {
-    return <String>[
-      "Recipes",
-      "Cooking",
-      "Culture",
-      "Shopping",
-      "Restaurants",
-      "BA Market",
-      "Videos",
-      "Podcast",
-      "Merch Shop"
+  // TODO Remove null and use actual page (use blank pages for now)
+  List<DrawerRoute> get routes {
+    return <DrawerRoute>[
+      DrawerRoute(text: "Recipes", pageRouteInfo: null),
+      DrawerRoute(text: "Cooking", pageRouteInfo: null),
+      DrawerRoute(text: "Culture", pageRouteInfo: null),
+      DrawerRoute(text: "Shopping", pageRouteInfo: null),
+      DrawerRoute(text: "Restaurants", pageRouteInfo: null),
+      DrawerRoute(text: "BA Market", pageRouteInfo: null),
+      DrawerRoute(text: "Videos", pageRouteInfo: null),
+      DrawerRoute(text: "Podcast", pageRouteInfo: null),
+      DrawerRoute(text: "Merch Shop", pageRouteInfo: null),
     ];
   }
 }
