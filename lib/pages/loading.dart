1d3edc40bcf1299aff1b062e9d9e54f7be74ed27
neretzes <caboyof@gmail.com>
2022-03-30 16:34:11 +0800

    Update loading.dart
    The dots are now animated 🥳 every second.

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,11 +1,43 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class LoadingPage extends StatelessWidget {
+class LoadingPage extends StatefulWidget {
   const LoadingPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: Make the ellipsis animated (hint: call setState with delays)
-    return const Scaffold(body: Center(child: Text("Loading...")));
+  State<LoadingPage> createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  int value = 1;
+
+  late final Timer timer;
+
+  @override
+  void initState() {
+    timer = cycleThroughValue();
+    super.initState();
   }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Loading$dots")));
+  }
+
+  Timer cycleThroughValue() {
+    return Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        value = (value + 1) % 4;
+      });
+    });
+  }
+
+  String get dots => "." * value;
 }
