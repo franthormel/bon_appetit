f96ba8da27e72ffa6271c99a40b512d7033dff3e
neretzes <caboyof@gmail.com>
2022-04-14 13:00:01 +0800

    Drawer page article widgets
    Finished widgets for cooking, shopping, and restaurants.

--- lib/widgets/drawer/page/articles/article.dart
+++ lib/widgets/drawer/page/articles/article.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 import '../../../../models/index.dart';
+import '../../../../router/index.dart';
+import '../../../../style/index.dart';
+import '../../../others/cached_hero_image.dart';
+import '../../../others/header_body/five.dart';
+import '../../../others/page_route_pusher.dart';
+import 'article_category.dart';
 
 class DrawerPageArticleWidget extends StatelessWidget {
   final Article article;
@@ -9,6 +15,34 @@ class DrawerPageArticleWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final indent = MediaQuery.of(context).size.width / 1.5;
+
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: PageRoutePusherWidget(
+        route: ArticleDetailsRoute(article: article),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CachedHeroImageWidget(
+              heroTag: article.heroTag,
+              imageUrl: article.imageUrl,
+            ),
+            const Divider(color: Colors.transparent),
+            DrawerPageArticleCategoryWidget(article.category),
+            const Divider(color: Colors.transparent),
+            HeaderBodyFiveWidget(
+              title: article.title,
+              body: article.subtitle,
+              dividerColor: BonAppetitColors.black,
+              dividerEndIndent: indent,
+            ),
+            const SizedBox(height: 40.0),
+            const Divider(color: BonAppetitColors.black),
+            const SizedBox(height: 10.0),
+          ],
+        ),
+      ),
+    );
   }
 }
