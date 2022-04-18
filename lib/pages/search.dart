43921f8781698fbeb287a78363380ae5ab335364
neretzes <caboyof@gmail.com>
2022-04-18 15:20:44 +0800

    Update search.dart

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -6,13 +6,20 @@ import '../widgets/index.dart';
 class SearchPage extends StatelessWidget {
   const SearchPage({Key? key}) : super(key: key);
 
+  // TODO: 2. Create provider for search page
+  // 1. Controller for TextField
+  // 2. Show by (All Content, Recipes, Articles, Videos)
+  // 3. Sort by (Relevance, newest, highest rating, most reviewed)
+
+  // TODO: 3. Separate widgets by functionality and for future use (different sizes and platforms)
+
   @override
   Widget build(BuildContext context) {
+    final textController = TextEditingController();
     final numbers = List.generate(40, (<int>(int i) => i));
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Search"),
         actions: const [
           SearchPageShowByTypeWidget(),
           SearchPageFilterWidget(),
@@ -28,10 +35,35 @@ class SearchPage extends StatelessWidget {
               padding: const EdgeInsets.all(16.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  TextField(),
-                  SizedBox(height: 8.0),
-                  Text("10,000 matching results"),
+                children: [
+                  TextField(
+                    controller: textController,
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      helperStyle: Theme.of(context)
+                          .textTheme
+                          .caption
+                          ?.copyWith(
+                              fontWeight: FontWeight.w400, letterSpacing: 0.4),
+                      helperText: "10,000 matching results",
+                      hintText: "Find a Recipe",
+                      prefixIcon: IconButton(
+                        color: BonAppetitColors.black,
+                        icon: const Icon(Icons.search),
+                        onPressed: () {},
+                      ),
+                      prefixIconColor: BonAppetitColors.black,
+                      suffixIcon: IconButton(
+                        color: BonAppetitColors.black,
+                        icon: const Icon(Icons.clear),
+                        onPressed: () {
+                          if (textController.text.isNotEmpty) {
+                            textController.clear();
+                          }
+                        },
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -39,6 +71,7 @@ class SearchPage extends StatelessWidget {
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: ListView.separated(
+                  // TODO: 1. Place results
                   itemBuilder: (context, index) => Text("${numbers[index]}"),
                   itemCount: numbers.length,
                   separatorBuilder: (context, index) =>
