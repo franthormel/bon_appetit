1841daaa0d03a8452c64bf25b2e282c368cd1308
neretzes <caboyof@gmail.com>
2022-04-18 10:19:44 +0800

    Search page widgets initial

---
+++ lib/widgets/search/results_count.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SearchPageResultsCountWidget extends StatelessWidget {
+  const SearchPageResultsCountWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/widgets/search/sort_by.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SearchPageSortByWidget extends StatelessWidget {
+  const SearchPageSortByWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
