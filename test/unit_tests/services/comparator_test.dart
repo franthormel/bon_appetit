1333f8768d722e284a936cf2f0fb98fb9b80beed
neretzes <caboyof@gmail.com>
2022-04-25 14:11:21 +0800

    Update comparator_test.dart
    Finished testing compareDoubleStrings()

--- test/unit_tests/services/comparator_test.dart
+++ test/unit_tests/services/comparator_test.dart
@@ -2,7 +2,7 @@ import 'package:bon_appetit/services/comparator.dart';
 import 'package:test/test.dart';
 
 class ComparatorServiceTester {
-  static void testCompareDoubleStrings(Map<String, String> map, Matcher m) {
+  static void testCompareDoubleStrings(Map<String?, String?> map, Matcher m) {
     map.forEach((a, b) {
       test("a=$a b=$b", () {
         expect(ComparatorService.compareDoubleStrings(a, b), m);
@@ -18,7 +18,7 @@ void main() {
         const m = isNegative;
 
         group("if a is greater than b", () {
-          const map = <String, String>{
+          const map = <String?, String?>{
             "5.0": "",
             "3.9": "3.",
             "1.1": "1.0",
@@ -27,8 +27,8 @@ void main() {
           ComparatorServiceTester.testCompareDoubleStrings(map, m);
         });
 
-        group("if b is null and a is not null", () {
-          const map = <String, String>{
+        group("if a is not null and b is null", () {
+          const map = <String?, String?>{
             "5.0": "",
             "3.9": "3.",
             "1.1": "1.0",
@@ -38,9 +38,59 @@ void main() {
         });
       });
 
-      group("should return 0", () {});
+      group("should return 0", () {
+        const m = isZero;
 
-      group("should return positive", () {});
+        group("if a and b are null", () {
+          ComparatorServiceTester.testCompareDoubleStrings({null: null}, m);
+        });
+
+        group("if a and b are not doubles", () {
+          const map = <String?, String?>{
+            "": "",
+            "*&*@": "KKJ(",
+            "asdnull": "nuller",
+            "nVll": "axr",
+            "ryh": "nUll",
+          };
+
+          ComparatorServiceTester.testCompareDoubleStrings(map, m);
+        });
+
+        group("if a and b are equal", () {
+          const map = <String?, String?>{
+            "3.0": "3.0",
+            "50.2": "50.20",
+            "0.12302": "00.12302",
+          };
+
+          ComparatorServiceTester.testCompareDoubleStrings(map, m);
+        });
+      });
+
+      group("should return positive", () {
+        const m = isPositive;
+
+        group("if a is lesser than b", () {
+          const map = <String?, String?>{
+            "": "1.2",
+            "3.2": "3.3",
+            "0.01": "1.01",
+            "5.1102": "5.11030",
+          };
+
+          ComparatorServiceTester.testCompareDoubleStrings(map, m);
+        });
+
+        group("if a is null and b is not null", () {
+          const map = <String?, String?>{
+            null: "3.2",
+            "null": "5.1",
+          };
+
+          ComparatorServiceTester.testCompareDoubleStrings(map, m);
+        });
+      });
     });
 
     group("compareIntStrings()", () {
