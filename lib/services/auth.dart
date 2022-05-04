3dc3070b343be09d361ef693e98c91a956405bd8
neretzes <caboyof@gmail.com>
2022-05-04 17:16:26 +0800

    Update auth.dart

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseAuthService {
-
+  // TODO: Implement email authentication
   bool checkIfEmailExists(String email) {
     return true;
   }
