ebefd65cc5d39ee6bad6eb8a9a34bb83a187eacd
Francis Anthony <caboyof@gmail.com>
2022-05-19 13:17:50 +0800

    Add initial files for provider and service

---
+++ lib/services/firebase/firestore.dart
@@ -0,0 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreService {
+  static FirebaseFirestore get _db => FirebaseFirestore.instance;
+
+  // TODO: Favorite recipe
+
+  // TODO: Unfavorite recipe
+
+  // TODO: Favorite article
+
+  // TODO: Unfavorite article
+
+  // TODO: Favorite video
+
+  // TODO: Unfavorite video
+}
---
+++ lib/services/providers/favorites.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class FavoritesProvider extends ChangeNotifier {
+  // TODO: Add favorite recipes list
+
+  // TODO: Add favorite articles list
+
+  // TODO: Add favorite videos list
+
+  // TODO: Add public getter for favorite recipes list
+
+  // TODO: Add public getter for favorite articles list
+
+  // TODO: Add public getter for favorite videos list
+
+  // TODO: Add method for adding a favorite recipe
+
+  // TODO: Add method for adding a favorite article
+
+  // TODO: Add method for adding a favorite video
+
+  // TODO: Add method for deleting a favorited recipe
+
+  // TODO: Add method for deleting a favorited article
+
+  // TODO: Add method for deleting a favorited video
+}
